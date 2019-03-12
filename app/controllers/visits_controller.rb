1e4d9b5bf8c3b79ff06d11aa84c349c4dc4d171e
Erick <egromero@uc.cl>
2019-03-12 14:17:24 -0300

    test error4

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -2,13 +2,13 @@ class VisitsController < ApplicationController
     skip_before_action :verify_authenticity_token
 
     def index
-        @laboratory = Laboratory.find(current_user.lab_id)
+        #@laboratory = Laboratory.find(current_user.lab_id)
         @visits_day = Visit.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-        @visits_day = @visits_day.where(lab_id: @laboratory.id)
+        #@visits_day = @visits_day.where(lab_id: @laboratory.id)
         @visits_week = Visit.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
-        @visits_week = @visits_week.where(lab_id: @laboratory.id)
+        #@visits_week = @visits_week.where(lab_id: @laboratory.id)
         @visits_month = Visit.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
-        @visits_month = @visits_month.where(lab_id: @laboratory.id)
+        #@visits_month = @visits_month.where(lab_id: @laboratory.id)
     end
 
     def new
@@ -23,7 +23,7 @@ class VisitsController < ApplicationController
     def create 
         st_id = Student.where(rut: params[:rut])[0]
         if st_id 
-            @record = st_id.records.new({:tipo => params[:tipo], :lab_id => params[:lab_id]})
+            @record = st_id.records.new({:tipo => params[:tipo]})
             respond_to do |format|
             if @record.save
                 format.json {render json: {'type': 'student', 'data': @record.student}, status: 200}
