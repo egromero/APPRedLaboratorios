1f44d2c0977299ce6a050a4ba409cfe23dfa4d68
Erick R <egromero@uc.cl>
2019-12-03 14:34:53 -0300

    chages to cancancan

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,17 +1,19 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: [:show, :edit, :update, :destroy]
   skip_before_action :verify_authenticity_token
   load_and_authorize_resource class: "Student"
+  before_action :authenticate, only:[:created_from_totem, :create]
+ 
 
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:warning] = "Acceso Denegado!"
+    redirect_to root_url
+    end
   # GET /students
   # GET /students.json
   def index
     @students = Student.all.order("id ASC")
   end
-  rescue_from CanCan::AccessDenied do |exception|
-    flash[:warning] = "Acceso Denegado!"
-    redirect_to root_url
-  end
+  
   # GET /students/1
   # GET /students/1.json
   def show
