8dc588695d2942e37a2b547cd3f7e461358697a8
Erick R <egromero@uc.cl>
2020-02-25 22:31:09 -0300

    orden en rutas y documentacion a gitignore

--- config/routes.rb
+++ config/routes.rb
@@ -1,45 +1,38 @@
 Rails.application.routes.draw do
-  resources :flayers
-  root 'home_page#index'
-
-  get "/slideshow" => "slideshow#slideshow"
-  devise_for :users
-  scope '/admin' do
-    resources :users
-    post 'users/new',to: "users#create"
-  end
   
+  root 'home_page#index'
+  
+  resources :flayers
   resources :laboratories
   resources :students
-  get 'home_page/index'
+  resources :records 
+  resources :visits
+  devise_for :users
 
   resource :students do 
     post 'enroll'
     post 'created_from_totem'
   end
-
-  get '/expulse', to: "records#expulse", as: "button"
-
-  resources :records 
-
-  resources :laboratories do
-    get 'new_admin'
-    post 'set_admin'
-  end
-
-
-  resources :visits
- 
-
-
-
+  
   resources :records do 
     collection {post :import}
   end
+  
   resources :visits do 
     collection {post :import}
   end
+  
   resources :students do 
     collection {post :import}
   end
+
+  scope '/admin' do
+    resources :users
+    post 'users/new',to: "users#create"
+  end
+
+  get "/slideshow" => "slideshow#slideshow"
+  get 'home_page/index'
+  get '/expulse', to: "records#expulse", as: "button"
+  
 end
