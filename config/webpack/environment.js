9eb60ca9f9d0c04caec897adfc079925a069e242
savelozo <savelozo@uc.cl>
2024-06-06 10:08:58 -0400

    delete dotenv

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,10 @@
 const { environment } = require('@rails/webpacker')
 const webpack = require('webpack')
-const dotenv = require('dotenv')
+//const dotenv = require('dotenv')
 
 // Cargar variables de entorno desde el archivo .env
-dotenv.config()
+//dotenv.config()
 
-environment.plugins.append('Environment', new webpack.EnvironmentPlugin(process.env))
+//environment.plugins.append('Environment', new webpack.EnvironmentPlugin(process.env))
 
 module.exports = environment
