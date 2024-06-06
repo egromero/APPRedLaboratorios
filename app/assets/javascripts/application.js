a84b16523e86a91c39fc608e1a6af854517f49d0
savelozo <savelozo@uc.cl>
2024-06-06 10:15:23 -0400

    remove post to AT

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -71,7 +71,7 @@ window.getRecords = function (id) {
   });
 };
 
-/* window.postForm = function () {
+window.postForm = function () {
   var data = {
     visit: {
       rut: document.getElementById("run").value,
@@ -96,58 +96,6 @@ window.getRecords = function (id) {
       }
       window.location.replace("/slideshow");
     },
-  });
-}; */
-
-window.postForm = function () {
-
-  console.log("Entrando a PostForm...")
-  var apiKey = 'patBExGhe6sVJtn6x.7ee85c8cd5d6afe6bdf4057a06a47dcf377af159ebc728268cd18ea365efb9ff'
-  /* process.env.API_KEY; */
-
-  var visit_data = {
-      rut: document.getElementById("run").value,
-      institucion: document.getElementById("institution").value,
-      lab_id: document.getElementById("lab").value,
-      motivo: document.getElementById("motivo").value,
-      other: document.getElementById("otro").value,
-      uc_student: document.getElementById("otro").value,
-      quantity: document.getElementById("counter").value,
-  };
-
-  var headers = {
-      'Authorization': 'Bearer '+ apiKey,
-      'Content-Type': 'application/json'
-  };
-
-  var data = {
-    'fields': {
-      'fldl99UjPoKChJyU1': visit_data.rut,
-      'fld4K10yQ9y74ryAO': visit_data.institucion,
-      'fldvLABQiqKIJPgK9': visit_data.lab_id,
-      'fldxNQvUYdFOf9x36': visit_data.motivo,
-      'fldBagNcAy9oSYo1n': visit_data.other,
-      'fldwFwYqPHLi5cqvE': visit_data.quantity,
-      'fldQDfXUY09GkKy1y': visit_data.uc_student
-    }
-  };
-  console.log(data)
-
-  $.ajax({
-      type: "POST",
-      url: "https://api.airtable.com/v0/appAva5Ns7QQbSSVn/tblQKTOunnX5STdms",
-      headers: headers,
-      data: JSON.stringify(data),
-      dataType: "json",
-      success: function (data) {
-          console.log('Success:', data);
-          alert("Visita registrada.");
-          window.location.replace("/slideshow");
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-          console.error('Error:', textStatus, errorThrown);
-          alert('Error al registrar la visita.');
-      }
   });
 };
 
