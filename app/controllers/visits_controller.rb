d8464f62a5211b168eb74fa1b607f830c74aca7b
savelozo <savelozo@uc.cl>
2024-06-05 22:21:48 -0400

    v10

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -60,78 +60,77 @@ class VisitsController < ApplicationController
     end
   end
 
-=begin class VisitsController < ApplicationController
-    skip_before_action :verify_authenticity_token
-    load_and_authorize_resource class: "Visit"
+# class VisitsController < ApplicationController
+#     skip_before_action :verify_authenticity_token
+#     load_and_authorize_resource class: "Visit"
 
-    rescue_from CanCan::AccessDenied do |exception|
-        flash[:warning] = "Acceso Denegado!"
-        redirect_to root_url
-      end
-    def index
-        @visits_day = Visit.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
-        @visits_week = Visit.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
-        @visits_month = Visit.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
-        @visit_all = Visit.all
-        @labs = Laboratory.all
-        respond_to do |format|
-            format.html
-            format.csv { send_data @visit_all.to_csv, filename: "visit-until-#{Date.today}.csv" }
-        end
-    end
-    def show
-        @labs = Laboratory.all
-    end
+#     rescue_from CanCan::AccessDenied do |exception|
+#         flash[:warning] = "Acceso Denegado!"
+#         redirect_to root_url
+#       end
+#     def index
+#         @visits_day = Visit.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+#         @visits_week = Visit.where(created_at: Time.zone.now.beginning_of_week..Time.zone.now.end_of_week)
+#         @visits_month = Visit.where(created_at: Time.zone.now.beginning_of_month..Time.zone.now.end_of_month)
+#         @visit_all = Visit.all
+#         @labs = Laboratory.all
+#         respond_to do |format|
+#             format.html
+#             format.csv { send_data @visit_all.to_csv, filename: "visit-until-#{Date.today}.csv" }
+#         end
+#     end
+#     def show
+#         @labs = Laboratory.all
+#     end
 
     
-    def new
-        @labs =  Laboratory.all
-        render layout: 'slideshow'
-        @visit = Visit.new
-    end
+#     def new
+#         @labs =  Laboratory.all
+#         render layout: 'slideshow'
+#         @visit = Visit.new
+#     end
 
-    def import
-        Visit.import(params[:file])
-        redirect_to root_path, notice: "Registros de visitas actualizados"
-    end
+#     def import
+#         Visit.import(params[:file])
+#         redirect_to root_path, notice: "Registros de visitas actualizados"
+#     end
     
-    def create
-        Rails.logger.info "Entrando a create..."
-        Rails.logger.info "Visit Params"
-        Rails.logger.info "visit_params"
-        @visit = Visit.new(visit_params)
+#     def create
+#         Rails.logger.info "Entrando a create..."
+#         Rails.logger.info "Visit Params"
+#         Rails.logger.info "visit_params"
+#         @visit = Visit.new(visit_params)
 
-        if @visit.save
-          redirect_to success_path, notice: 'Visita registrada exitosamente.'
-        else
-          render layout: 'slideshow' 
-        end
+#         if @visit.save
+#           redirect_to success_path, notice: 'Visita registrada exitosamente.'
+#         else
+#           render layout: 'slideshow' 
+#         end
 
-        student = Student.where(rut: visit_params[:rut])[0]
-        if student 
-            if student.status.nil?
-                student.status = true
-                student.save
-            end
-            @record = student.records.new({:tipo => student.status, :lab_id =>visit_params[:lab_id]})
-            if @record.save
-                student.status = !student.status
-                student.save
-                render json: {type: "student", name: student.nombre}
-            end
-        else    
-            @visit = Visit.new(rut: visit_params[:rut], motivo: visit_params[:motivo], institucion: visit_params[:institucion], lab_id: visit_params[:lab_id], other: visit_params[:other], quantity: visit_params[:quantity])
-            if @visit.save
-                render json: {type: "visit"}
-            end
-        end
-    end 
+#         student = Student.where(rut: visit_params[:rut])[0]
+#         if student 
+#             if student.status.nil?
+#                 student.status = true
+#                 student.save
+#             end
+#             @record = student.records.new({:tipo => student.status, :lab_id =>visit_params[:lab_id]})
+#             if @record.save
+#                 student.status = !student.status
+#                 student.save
+#                 render json: {type: "student", name: student.nombre}
+#             end
+#         else    
+#             @visit = Visit.new(rut: visit_params[:rut], motivo: visit_params[:motivo], institucion: visit_params[:institucion], lab_id: visit_params[:lab_id], other: visit_params[:other], quantity: visit_params[:quantity])
+#             if @visit.save
+#                 render json: {type: "visit"}
+#             end
+#         end
+#     end 
 
-    def visit_params
-        params.require(:visit).permit(:rut, :motivo, :institucion, :lab_id, :other, :quantity, :uc_student)
-    end
+#     def visit_params
+#         params.require(:visit).permit(:rut, :motivo, :institucion, :lab_id, :other, :quantity, :uc_student)
+#     end
 
 
-end
+# end
 
- =end
